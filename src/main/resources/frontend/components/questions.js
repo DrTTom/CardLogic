be2d7ad0db630a83c8ee4348182c3c1e85c57112
DrTTom <me@example.com>
2017-03-10 14:49:59 +0100

    cleanup

--- src/main/resources/frontend/components/questions.js
+++ src/main/resources/frontend/components/questions.js
@@ -15,11 +15,6 @@ Vue.component('questions', {
             required:false,
             default:[]
         },
-        questionGroup: {
-            type:Array,
-            required:false,
-            default:[]
-        },
         currentQuestion: {
             type: Object,
             required: false,
@@ -37,53 +32,33 @@ Vue.component('questions', {
             this.questionGroup=[];
             this.questionGroups=[];
             var currentGroup= { name:'', elements:[] };
-            for (i=0; i< this.allQuestions.length; i++)
-            {
-            	if (this.allQuestions[i].form!=currentGroup.name)
-            	{
-             	currentGroup= { name:this.allQuestions[i].form, elements:[] };
-            	this.questionGroups.push(currentGroup);
-            	 console.log("pushed group");
-             	}
-             currentGroup.elements.push(this.allQuestions[i]);	
-             }
-            var that=this;
-            this.allQuestions.forEach(function(q){
-            	 if (q.form=='identify')
-            		 {
-            	that.questionGroup.push(q);
-            		 }
-            });
-        },
+            for (i=0; i< this.allQuestions.length; i++) {
+               if (this.allQuestions[i].form!=currentGroup.name) {
+                  currentGroup.next=this.allQuestions[i].form;
+                  currentGroup= { name:this.allQuestions[i].form, prev: currentGroup.name, elements:[] };
+                  this.questionGroups.push(currentGroup);
+                  }
+               currentGroup.elements.push(this.allQuestions[i]);	
+               }
+            },
         answerQuestion: function (event) {
-            if (actorUser==true)
-            {
-            actorUser=false;        
-        	if (event.which == 13 || event.keyCode == 13 || event.type == 'change') {
-        	console.log("will re-send answers");
-                CardEvents.answerQuestion.send(this.allQuestions);
-            }
-            }
-           // if (event.which == 0 || event.keyCode == 9)  for tab
-        },
-       enableUpdate: function(event)
-       {
+            if (actorUser==true) {
+               actorUser=false;
+               if (event.which == 13 || event.keyCode == 13 || event.type == 'change') {
+                  CardEvents.answerQuestion.send(this.allQuestions);
+                  }
+               }
+            // if (event.which == 0 || event.keyCode == 9)  for tab
+            },
+       enableUpdate: function(event) {
           actorUser=true;
           console.log("User interaction detected");
        },
-       example: function(event) 
-       {
-    	   var dummy={"x": "hallo", "y": "tt"};
-    	   this.$http.post(url+'/submit', dummy).then( (response) => {
-          	 console.log("should have sent");
-            } );
-       },
-       getKeys : function(obj){
-        	   var keys = [];
-        	   for(var key in obj){
-        	      keys.push(key);
-        	   }
-        	   return keys;
-        	}
+       store: function(event) {
+          var dummy={"x": "hallo", "y": "tt"};
+          this.$http.post(url+'/submit', dummy).then( (response) => {
+             console.log("should have sent");
+             });
+          }
     }
 })
